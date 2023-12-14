bcbac773950ae25e9c9656e135cb922bb7ba35ad
ahmedmuhammed <ahmedmuhammedem@gmail.com>
2023-12-14 10:51:01 +0300

    Implement hover effect for Etch-a-Sketch grid
    Add hover effect to grid divs
    Changes color of grid divs on mouse hover, simulating a pen trail
    Achieve pixelated trail effect for a more interactive experience
    Update CSS styles to handle the hover effect gracefully

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Etch-a-Sketch
+
+This simple web application allows you to draw colorful sketches by hovering your mouse over a grid of squares. You can customize the size of the grid for a more detailed or abstract drawing experience.
+
+## Features
+
+- **Grid Drawing:** Hover over the squares in the grid to draw colorful patterns.
+- **Random Colors:** Each square changes its color randomly as you hover over it.
+- **Grid Size Customization:** Click the "Enter No of box" button to enter a new number of squares per side for the grid.
+
+## How to Use
+
+1. Visit the [Etch-a-Sketch GitHub Page](https://github.com/amswdeveloper/Etch-a-Sketch) in your web browser.
+2. The initial grid size is set to 4 squares per side.
+3. Hover over the squares to draw, and enjoy the pixelated trail effect.
+4. To change the grid size, click the "Enter No of box" button, enter a number between 1 and 100 in the prompt, and see the grid update accordingly.
+
+## Code Overview
+
+The main functionality is implemented in JavaScript, and the code structure is as follows:
+
+- **HTML (`index.html`):** Contains the structure of the webpage, including a container for the grid and a button to change the grid size.
+- **CSS (`style.css`):** Provides minimal styling for the grid and button.
+
+- **JavaScript (`script.js`):**
+  - `createGrid`: Dynamically generates a grid of squares based on the specified number of squares per side.
+  - `handleSquareHover`: Changes the background color of a square to a random color on mouseover.
+  - `getRandomColor`: Generates a random hex color code.
+  - Event listeners are set up to handle mouseover events on the grid squares and button click events to change the grid size.
--- script.js
+++ script.js
@@ -1,97 +1,43 @@
 const container = document.querySelector(".container");
 const button = document.querySelector(".btn");
 
-const color = [
-  "#c92a2a",
-  "#c2255c",
-  "#212529",
-  "#087f5b",
-  "#5f3dc4",
-  "#fa5252",
-  "#ae3ec9",
-  "#f76707",
-  "#c92a2a",
-  "#63e6be",
-  "#212529",
-  "#33220f",
-  "#862e9c",
-  "#fa5252",
-  "#0b7285",
-  "#1864ab",
-  "#fa5252",
-  "#c92a2a",
-  "#63e6be",
-  "#212529",
-  "#33220f",
-  "#862e9c",
-  "#fa5252",
-  "#0b7285",
-  "#1864ab",
-  "#fa5252",
-  "#c92a2a",
-  "#63e6be",
-  "#212529",
-  "#33220f",
-  "#862e9c",
-  "#fa5252",
-  "#0b7285",
-  "#1864ab",
-  "#fa5252",
-  "#c92a2a",
-  "#63e6be",
-  "#212529",
-  "#33220f",
-  "#862e9c",
-  "#fa5252",
-  "#0b7285",
-  "#1864ab",
-  "#fa5252",
-  "#c92a2a",
-  "#63e6be",
-  "#212529",
-  "#33220f",
-  "#862e9c",
-  "#fa5252",
-  "#0b7285",
-  "#1864ab",
-  "#fa5252",
-  "#1864ab",
-  "#fa5252",
-  "#c92a2a",
-  "#63e6be",
-  "#212529",
-  "#33220f",
-  "#862e9c",
-  "#fa5252",
-  "#0b7285",
-  "#1864ab",
-  "#fa5252",
-  "#862e9c",
-];
 let result = [];
 
-function getDivElement(numberOfSquares) {
+function createGrid(numberOfSquares) {
+  container.textContent = "";
   let width = 600 / numberOfSquares;
-  let box = document.createElement("div");
 
   for (let i = 1; i <= numberOfSquares * numberOfSquares; i++) {
-    box = document.createElement("div");
+    let box = document.createElement("div");
     box.style.width = `${width}px`;
     box.style.height = `${width}px`;
-    box.style.backgroundColor = `${color[i]}`;
+    box.addEventListener("mouseover", handleSquareHover);
     result.push(box);
   }
-
   return result;
 }
 
-container.append(...getDivElement(4));
+function handleSquareHover() {
+  this.style.backgroundColor = getRandomColor();
+}
 
-button.addEventListener("click", () => {
-  const numberOfSquares = Number(
-    prompt("How many number of squares per side do you want? ")
-  );
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-  width = 600 / numberOfSquares;
-  container.append(...getDivElement(numberOfSquares));
+container.append(...createGrid(4));
+
+button.addEventListener("click", function () {
+  const input = Number(prompt("Enter number of square per side do you want? "));
+  if (!isNaN(input) && input > 0 && input <= 100) {
+    result = [];
+    container.append(...createGrid(input));
+  } else {
+    alert("Please enter the number between 1 and 100.");
+  }
 });
